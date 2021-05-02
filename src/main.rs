0bafa1c22a3fb33ec9fadd9a56e1775c7a9e0f93
D. Puplauskas <puplauskas@hotmail.com>
2021-05-02 13:39:36 +0200

    greeting people using query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_startup_system(add_people.system())
+        .add_system(hello_world.system())
+        .add_system(greet_people.system())
+        .run();
+}
+
+struct Person;
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Ash".to_string()));
+    commands.spawn().insert(Person).insert(Name("Billy".to_string()));
+    commands.spawn().insert(Person).insert(Name("Clemens".to_string()));
+}
+
+fn hello_world() {
+    println!("hello world");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
 }
